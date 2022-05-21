fe3b5593f668d4c455dc6208e2ed62dd54ef5c71
sara bouglam <bouglamsara@gmail.com>
2022-05-21 23:38:29 +0200

    Plotting th eobtained results

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import argparse
+import os
 
 import source.config as config
 from source.generate_data import TrainingDataGenerator
 from source.trainer import SegNetTrainer
 from source.segnet_model import SegNet
-from source.test_model import  TestModel
+from source.test_model import TestModel
 
 import tensorflow as tf
 import keras.backend as K
@@ -16,14 +17,22 @@ class ModeTrainer:
     def generate_data():
         data_generator = TrainingDataGenerator(images_path=config.images_dataset_path,
                                                masks_path=config.masks_dataset_path)
-        #data_generator.execute()
+        # data_generator.execute()
         data_generator.generate_files_per_set()
 
-    def test_segnet(self):
+    @staticmethod
+    def test_segnet(image):
         model_tester = TestModel(config.model_path)
-        image_path = "/home/sara/Documents/techniques_of_artificial_intelligence/INFO-H410-Project/data" \
-                     "/preprocessed_data/training_set/image125.nii "
-        predict = model_tester.predict(image_path)
+        image_name = image + ".nii"
+        mask_name = image + "_segmentation.nii"
+
+        image_path = os.path.join(config.test_set_path, image_name)
+        mask_path = os.path.join(config.test_set_path, mask_name)
+
+        model_tester.predict(image_path, mask_path)
+    @staticmethod
+    def show_evolution(path_to_metrics):
+        TestModel.display_training_evolution(path_to_metrics)
 
     @staticmethod
     def multi_class_dice_score(ground_truth, prediction):
@@ -51,7 +60,7 @@ class ModeTrainer:
         optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4)
 
         segnet_trainer.train(model=model,
-                  loss_func=loss_func, accuracy_func=accuracy_func, optimizer=optimizer)
+                             loss_func=loss_func, accuracy_func=accuracy_func, optimizer=optimizer)
 
 
 if __name__ == "__main__":
@@ -59,6 +68,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--preprocess', action='store_true', required=False)
     parser.add_argument('--train', action='store_true', required=False)
+    parser.add_argument('--show_evol', action='store_true', required=False)
     parser.add_argument('--test', action='store_true', required=False)
 
     arguments = parser.parse_args()
@@ -70,4 +80,6 @@ if __name__ == "__main__":
     if arguments.train:
         model_trainer.train_segnet()
     if arguments.test:
-        model_trainer.test_segnet()
+        model_trainer.test_segnet("image647")
+    if arguments.show_evol:
+        model_trainer.show_evolution(config.metrics_path)
--- source/test_model.py
+++ source/test_model.py
@@ -4,7 +4,8 @@ from tensorflow import keras
 import SimpleITK as sitk
 import numpy as np
 import matplotlib.pyplot as plt
-
+from matplotlib.lines import Line2D
+import pandas as pd
 
 class TestModel:
     def __init__(self, path_to_model):
@@ -12,22 +13,57 @@ class TestModel:
                                              custom_objects=None, compile=False, options=None
                                              )
 
-    def predict(self, path_to_image):
-        image = self.__get_image(path_to_image)
-        # image = np.expand_dims(image, axis=-1)
+    def predict(self, path_to_image, path_to_mask):
+        image_ = self.__get_image(path_to_image)
+        mask = self.__get_image(path_to_mask)
 
-        image = np.expand_dims(image, axis=0)
+        image = np.expand_dims(image_, axis=0)
         predicted = self.model.predict(image)
 
         predicted = np.argmax(predicted, axis=-1)
-        plt.figure()
-        plt.imshow((predicted)[0])
-        plt.show()
+        cmap = plt.cm.viridis
+        custom_lines = [Line2D([0], [0], color=cmap(1.), lw=4),
+                        Line2D([0], [0], color=cmap(0.75), lw=4),
+                        Line2D([0], [0], color=cmap(0.50), lw=4),
+                        Line2D([0], [0], color=cmap(0.25), lw=4), ]
 
-        return predicted
+        plt.subplot(2, 1, 1)
+        plt.title("Ground truth")
+        plt.imshow(image_, cmap='gray')
+        plt.imshow(mask, alpha=0.6, cmap='viridis')
+        plt.legend(custom_lines, ['Pancreas', 'spleen', 'Kidney', 'liver'], bbox_to_anchor=(1.04, 1), loc="upper left")
+
+        plt.subplot(2, 1, 2)
+        plt.title("Predicted")
+        plt.imshow(image_, cmap='gray')
+        plt.imshow(predicted[0], alpha=0.6, cmap='viridis')
+        plt.show()
 
     @staticmethod
     def __get_image(path):
         sitk_image = sitk.ReadImage(path)
         image_arr = sitk.GetArrayFromImage(sitk_image)
         return image_arr
+
+    @staticmethod
+    def display_training_evolution(path_to_metrics):
+        df = pd.read_csv(path_to_metrics)
+        epoch = (df["epoch"]).tolist()
+        train_loss = (df["train_loss"]).tolist()
+        val_loss = (df["val_loss"]).tolist()
+        accuracy = (df["accuracy"]).tolist()
+
+        plt.figure()
+        plt.plot(epoch, train_loss, label="Train loss")
+        plt.plot(epoch, val_loss, label="Val loss")
+        plt.plot(epoch, accuracy, label="Accuracy")
+        plt.legend()
+        plt.show()
+
+
+
+
+
+
+
+
